682a3e919224a9ae7cd12eb76ee69f4357670c2d
katerinajoanna <kasiapotar@gmail.com>
2024-10-16 23:40:20 +0200

    Fixat lite SetTimer

--- src/pages/settimer/SetTimer.jsx
+++ src/pages/settimer/SetTimer.jsx
@@ -2,16 +2,32 @@ import './setTimer.css';
 import Btn from '../../components/button/Btn';
 import vectorLeft from '../../assets/vectorLeft.svg';
 import vectorRigth from '../../assets/vectorRigth.svg';
+import { useState } from 'react';
 
 function SetTimer() {
+    const [minutes, setMinutes] = useState(0);
+
+    const incrementMinutes = () => {
+        if (minutes < 99) {
+            setMinutes(minutes + 1)
+        }
+    };
+
+    const decrementMinutes = () => {
+        if (minutes > 0) {
+            setMinutes(minutes - 1)
+        }
+    };
+
+
     return (
         <div className="set-timer">
             <section className="set-time">
-                <figure className="vector-left">
+                <figure className="vector-left" onClick={decrementMinutes}>
                     <img src={vectorLeft} alt="vector left" />
                 </figure>
-                <h1>00</h1>
-                <figure className="vector-rigth">
+                <h1>{minutes.toString().padStart(2, '0')}</h1>   {/*vissar minuter i format 00 */}
+                <figure className="vector-rigth" onClick={incrementMinutes}>
                     <img src={vectorRigth} alt="vector rigth" />
                 </figure>
             </section>
