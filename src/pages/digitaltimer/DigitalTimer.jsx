2aaa0a1b8513210b68cd81f48ae087396045cd3d
katerinajoanna <kasiapotar@gmail.com>
2024-10-17 13:31:27 +0200

    Fixat btn.css, digitaltimer

--- src/pages/digitaltimer/DigitalTimer.jsx
+++ src/pages/digitaltimer/DigitalTimer.jsx
@@ -1,9 +1,28 @@
 import './digitalTimer.css';
+import navicon from '../../assets/navicon.svg';
+import Btn from '../../components/button/Btn';
+import { useNavigate } from 'react-router-dom';
+
 
 function DigitalTimer() {
+    const navigate = useNavigate();
+
+    const abortTimer = () => {
+        navigate('/set-timer');
+    }
+
     return (
-        <div>
-            timer
+        <div className='digital'>
+            <section className="digital-nav">
+                <img src={navicon} alt="nav icon" />
+                <p>interval</p>
+            </section>
+            <section className="digital-timer">
+                <p>00:00</p>
+            </section>
+            <section className="digital-btn">
+                <Btn text="ABORT TIMER" onClick={abortTimer} />
+            </section>
         </div>
     )
 };
