b5ea4653838ad1be533d730be6679bf4e73f5e7b
katerinajoanna <kasiapotar@gmail.com>
2024-10-18 13:36:06 +0200

    Adding an icon to the Menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import './menu.css';
 import { motion } from 'framer-motion';
+import navicon from '../../assets/navicon.svg';
 
 function Menu({ onSelect }) {
-    // definera animation
+
+    const [isIconClicked, setIsIconClicked] = useState();
+
+    // definera animation för elem.
     const menuItemVariants = {
         hidden: {
             opacity: 0,
@@ -18,12 +23,56 @@ function Menu({ onSelect }) {
         }),
     };
 
+    //initial transparens för icon
+    const iconVariants = {
+        hidden: { opacity: 0, x: 100, filter: 'invert(0.8)' },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                duration: 0.4,
+                delay: 0.2,
+            },
+        },
+        clicked: {
+            x: 0,
+            rotate: 360,
+            filter: 'invert(0.5)',
+            scale: 0.8,
+            transition: {
+                duration: 0.9,
+                ease: 'linear',
+            },
+        },
+
+    };
+
     const handleSelect = (option) => {
         onSelect(option);
     };
 
+    // function för icon
+    const handleIconClick = () => {
+        setIsIconClicked(true);
+        setTimeout(() => {
+            handleSelect('close');     // för att stänga
+            setIsIconClicked(false);
+        }, 400);    // ms för att sluta animation
+    };
+
+
     return (
         <div className='menu'>
+            <motion.img
+                src={navicon}
+                alt="nav icon"
+                className="img"
+                variants={iconVariants}
+                initial="hidden"
+                animate={isIconClicked ? 'clicked' : 'visible'}    //function att stänga anim
+                onClick={handleIconClick}
+                style={{ filter: isIconClicked ? 'invert(1)' : 'invert(0)' }}
+            />
             {['digital', 'analog', 'visual'].map((option, index) => (
                 <motion.h1
                     key={option}
